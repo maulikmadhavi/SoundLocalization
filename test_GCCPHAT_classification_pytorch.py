a2a840cb65cc4b60a48dd010fc776f91eac219c7
maulikmadhavi <maulikmadhavi@gmail.com>
2018-12-27 19:18:11 +0800

    GCCPHAT classification and regression

--- test_GCCPHAT_classification_pytorch.py
+++ test_GCCPHAT_classification_pytorch.py
@@ -28,6 +28,14 @@ data = hdf5storage.loadmat("Training_lab.mat")
 Ytr = data['Ytr']       # Binary target
 Itr = data['Itr']       # indicator for single or 2 source
 Ztr = data['Ztr']   # Gaussian-format target
+# Since multi-target is not supported in classification problem, discard all
+# the features corresponding to multiple or 0 target
+y = np.where(Itr == 1)
+Xtrr = Xtr[y[0], :]
+Ytrr = Ytr[y[0], :]
+Ztrr = Ztr[y[0], :]
+Itrr = Itr[y[0], :]
+ytrr = np.where(Ytrr == 1)[1]  # target index
 
 
 data = hdf5storage.loadmat("Testing1_GCCPHAT_SSLR.mat")
@@ -37,7 +45,12 @@ data = hdf5storage.loadmat("Testing1_lab.mat")
 Yte1 = data['Yte1']  # Binary target
 Ite1 = data['Ite1']  # indicator for single or 2 source
 Zte1 = data['Zte1']  # Gaussian-format target
-
+y = np.where(Ite1 == 1)
+Xte1r = Xte1[y[0], :]
+Yte1r = Yte1[y[0], :]
+Zte1r = Zte1[y[0], :]
+Ite1r = Ite1[y[0], :]
+yte1r = np.where(Yte1r == 1)[1]  # target index
 
 data = hdf5storage.loadmat("Testing2_GCCPHAT_SSLR.mat")
 Xte2 = data['Xte2']
@@ -46,10 +59,19 @@ data = hdf5storage.loadmat("Testing2_lab.mat")
 Yte2 = data['Yte2']  # Binary target
 Ite2 = data['Ite2']  # indicator for single or 2 source
 Zte2 = data['Zte2']  # Gaussian-format target
+yte2 = np.where(Yte2 == 1)[1]  # target index
+y = np.where(Ite2 == 1)
+Xte2r = Xte2[y[0], :]
+Yte2r = Yte2[y[0], :]
+Zte2r = Zte2[y[0], :]
+Ite2r = Ite2[y[0], :]
+yte2r = np.where(Yte2r == 1)[1]  # target index
 
 
 ##############################################################################################
 # Create custom loader
+
+
 class MyDataloaderClass(Dataset):
 
     def __init__(self, X_data, Y_data):
@@ -64,7 +86,7 @@ class MyDataloaderClass(Dataset):
         return self.len
 
 
-train_loader_obj = MyDataloaderClass(Xtr, Ztr)
+train_loader_obj = MyDataloaderClass(Xtrr, yy)  # Use soft labels
 train_loader = DataLoader(dataset=train_loader_obj, batch_size=256, shuffle=True, num_workers=1)
 
 
@@ -99,7 +121,7 @@ class Model(torch.nn.Module):
         x1 = F.relu(self.dense1_bn(self.linear1(x)))
         x2 = F.relu(self.dense2_bn(self.linear2(x1)))
         x3 = F.relu(self.dense3_bn(self.linear3(x2)))
-        y_pred = F.sigmoid(self.linear4(x3))
+        y_pred = self.linear4(x3)
         return y_pred
 
 
@@ -121,39 +143,37 @@ def training(epoch):
                                                                            len(train_loader.dataset), 100. * batch_idx / len(train_loader), loss.data[0]))
 
 
-def testing(Xte, Yte, Ite):
+def testing(Xte, yte):
     model.eval()
     MAE = []
     Y_pred_t = model.forward(torch.from_numpy(Xte))
 
     for i in range(Xte.shape[0]):
         hyp = Y_pred_t[i].detach().numpy()
-        y_1 = Yte[i]
+        a1 = yte[i]
+        b1 = np.argmax(hyp)
+        ang = angular_distance_compute(a1, b1)
+        MAE.append(ang)
 
-        if Ite[i] == 1:
-            a1 = np.where(y_1 == 1)[0]
-            b1 = np.argmax(hyp)
-            ang = angular_distance_compute(a1, b1)[0]
-            MAE.append(ang)
     return sum(MAE) / len(MAE)
+
 
 
 # our model
 model = Model()
 
-criterion = torch.nn.MSELoss(size_average=True)
+criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters())
-
 
 for epoch in range(20):
     # Train
     training(epoch)
     # save model
-    modelname = '3layerMLPregre_epoch' + str(epoch) + '.model'
+    modelname = '3layerMLPclass_epoch' + str(epoch) + '.model'
     torch.save(model.state_dict(), modelname)
     # Do testing
-    mae1 = testing(Xte1, Yte1, Ite1)
-    mae2 = testing(Xte2, Yte2, Ite2)
+    mae1 = testing(Xte1r, yte1r)
+    mae2 = testing(Xte2r, yte2r)
 
     print('MAE-set1 =', '{:.3f}'.format(mae1),
           'MAE-set2 =', '{:.3f}'.format(mae2))
